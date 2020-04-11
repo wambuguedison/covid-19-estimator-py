d0bf57000a7a6898223f998a84644f4598b1a4a7
Eddie Waweru <wambuguedison@gmail.com>
2020-04-12 00:14:26 +0300

    xml post function

--- backendAPI/app.py
+++ backendAPI/app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, request
+from dicttoxml import dicttoxml
 
 from src.estimator import estimator
 
@@ -28,3 +29,21 @@ def json_api():
   }
   
   return jsonify(estimator(data))
+
+@app.route('/api/v1/on-covid-19/xml', methods=['POST'])
+def xml_api():
+  data = {
+    "region": {
+      "name": request.json['region']['name'],
+      "avgAge": request.json['region']['avgAge'],
+      "avgDailyIncomeInUSD": request.json['region']['avgDailyIncomeInUSD'],
+      "avgDailyIncomePopulation": request.json['region']['avgDailyIncomePopulation']
+    },
+    "periodType": request.json['periodType'],
+    "timeToElapse": request.json['timeToElapse'],
+    "reportedCases": request.json['reportedCases'],
+    "population": request.json['population'],
+    "totalHospitalBeds": request.json['totalHospitalBeds']
+  }
+  
+  return dicttoxml(estimator(data))
