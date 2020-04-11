782d5ce41f9ae33daadf569839b5d98e572508b2
Eddie Waweru <wambuguedison@gmail.com>
2020-04-11 14:24:49 +0300

    heroku configuration

--- backendAPI/app.py
+++ backendAPI/app.py
@@ -3,9 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])z
 def index():
   return "This is the covid 19 estimator api"
-  
-if __name__ == '__main__':
-  app.run(debug=True)
